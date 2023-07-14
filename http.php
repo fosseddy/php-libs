f52997dce51e1d127b13efdc6a90378b89fdcd5e
fosseddy <markfosseddy@gmail.com>
2023-07-14 11:15:49 +0500

    ready, set, go!

---
+++ http.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace http;
+
+use Exception;
+
+class Error extends Exception
+{
+    public $status_code;
+    public $data;
+
+    function __construct(int $status_code, string $message = "",
+                         array $data = []) {
+        parent::__construct($message);
+
+        $this->status_code = $status_code;
+        $this->data = $data;
+    }
+
+    function getData(): array
+        return ["message" => $this->message, ...$this->data];
+    }
+}
+
+class Bad_Request extends Error
+{
+    public $status_code = 400;
+
+    function __construct(string $message = "", array $data = []) {
+        parent::__construct($this->status_code, $message, $data);
+    }
+}
+
+class Not_Found extends Error
+{
+    public $status_code = 404;
+
+    function __construct(string $message = "", array $data = []) {
+        parent::__construct($this->status_code, $message, $data);
+    }
+}
+
+class Unsupported_Media_Type extends Error
+{
+    public $status_code = 415;
+
+    function __construct(string $message = "", array $data = []) {
+        parent::__construct($this->status_code, $message, $data);
+    }
+}
---
+++ path.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace path;
+
+// art: BASE_DIR must be defined by user
+function from_base(string ...$path): string
+{
+    return join(DIRECTORY_SEPARATOR, [BASE_DIR, ...$path]);
+}
